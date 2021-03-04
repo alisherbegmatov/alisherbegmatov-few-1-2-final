b7885a449c4ad0724ab1fa5c8bb65f8f62566d4d
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2021-03-03 16:22:42 -0800

    Completed All Challenges
    Completed All Challenges

---
+++ src/Heading.js
@@ -0,0 +1,13 @@
+import { Paper, Typography } from "@material-ui/core"
+import React from "react"
+
+function Heading() {
+  return (
+    <Paper>
+    <Typography>🤘🏻 Alisher Begmatov 🤘🏻</Typography>
+    <Typography>FEW 1.2 Final Assessment</Typography>
+    </Paper>
+  )
+}
+
+export default Heading
